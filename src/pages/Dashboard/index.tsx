830eabebafc26c36808abc2164563e379ef88555
renzivan <renzivanyapenguio@gmail.com>
2024-07-09 03:22:19 +0800

    minor cleanup

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,14 @@
 export function Dashboard() {
   return (
-    <>
-      <div>Dashboard</div>
-    </>
+    <div>
+      <div className="flex justify-center">
+        <div>Example Player ID: 131933530</div>
+      </div>
+      <div className="flex justify-center gap-2">
+        <a className="hover:underline" target="_blank" href="https://github.com/renzivan/reactjs-dota2-profile-tracker">Source Code Repository</a>
+        <span>|</span>
+        <a className="hover:underline" target="_blank" href="mailto:renzivanyapenguio@gmail.com">renzivanyapenguio@gmail.com</a>
+      </div>
+    </div>
   )
 }
