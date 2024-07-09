007af05ab9bcf75639f2129dbfacab370a04b4e2
renzivan <renzivanyapenguio@gmail.com>
2024-07-09 17:23:23 +0800

    fixed rankTier display for higher ranks

--- src/components/RankTier/index.tsx
+++ src/components/RankTier/index.tsx
@@ -1,7 +1,17 @@
-export default function RankTier(props: { rank: number }) {
+export default function RankTier(props: { rank: number, leaderBoard: number }) {
 
   const getRankImage = () => {
-    if (!props.rank) return '/ranks/rank_icon_0.png'
+    if (!props.rank) {
+      return '/ranks/rank_icon_0.png'
+    }
+
+    if (props.leaderBoard > 10 && props.leaderBoard <= 100) {
+      return '/ranks/rank_icon_8b.png'
+    }
+
+    if (props.leaderBoard <= 10) {
+      return '/ranks/rank_icon_8c.png'
+    }
 
     return `/ranks/rank_icon_${props.rank.toString()[0]}.png`
   }
@@ -13,9 +23,18 @@ export default function RankTier(props: { rank: number }) {
   }
 
   return (
-    <div>
-      {props?.rank < 80 && <img src={getTierImage()} alt="Tier" />}
-      <img src={getRankImage()} alt="Rank" width={50} />
+    <div className="relative mt-4">
+      {props?.rank < 80 &&
+        <div className="w-28 absolute">
+          <img src={getTierImage()} alt="Tier" />
+        </div>
+      }
+      <div className="w-28">
+        <img src={getRankImage()} alt="Rank" className="object-cover"  />
+      </div>
+      {props?.leaderBoard &&
+        <div className="font-bold text-amber-200 absolute bottom-2 left-0 right-0 flex justify-center">{props.leaderBoard}</div>
+      }
     </div>
   )
 }
