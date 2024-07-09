49be094d091730b0bfc83c6c88b679a3f5022ddd
renzivan <renzivanyapenguio@gmail.com>
2024-07-09 22:31:25 +0800

    updated ranktier component

--- src/components/RankTier/index.tsx
+++ src/components/RankTier/index.tsx
@@ -1,39 +1,49 @@
-export default function RankTier(props: { rank: number, leaderBoard: number }) {
+type RankTierProps = {
+  rank: number
+  leaderBoard: number
+  width?: number
+}
+
+export default function RankTier({
+  rank,
+  leaderBoard,
+  width = 28
+}: RankTierProps) {
 
   const getRankImage = () => {
-    if (!props.rank) {
+    if (!rank) {
       return '/ranks/rank_icon_0.png'
     }
 
-    if (props.leaderBoard > 10 && props.leaderBoard <= 100) {
+    if (leaderBoard > 10 && leaderBoard <= 100) {
       return '/ranks/rank_icon_8b.png'
     }
 
-    if (props.leaderBoard <= 10) {
+    if (leaderBoard <= 10) {
       return '/ranks/rank_icon_8c.png'
     }
 
-    return `/ranks/rank_icon_${props.rank.toString()[0]}.png`
+    return `/ranks/rank_icon_${rank.toString()[0]}.png`
   }
 
   const getTierImage = () => {
-    if (!props.rank) return null
+    if (!rank) return null
 
-    return `/ranks/rank_star_${props.rank.toString()[1]}.png`
+    return `/ranks/rank_star_${rank.toString()[1]}.png`
   }
 
   return (
-    <div className="relative mt-4">
-      {props?.rank < 80 &&
-        <div className="w-28 absolute">
+    <div className="relative">
+      {(rank < 80 && rank % 10 !== 0) &&
+        <div className={`w-${width} absolute`}>
           <img src={getTierImage()} alt="Tier" />
         </div>
       }
-      <div className="w-28">
+      <div className={`w-${width}`}>
         <img src={getRankImage()} alt="Rank" className="object-cover"  />
       </div>
-      {props?.leaderBoard &&
-        <div className="font-bold text-amber-200 absolute bottom-2 left-0 right-0 flex justify-center">{props.leaderBoard}</div>
+      {leaderBoard &&
+        <div className="font-bold text-amber-200 absolute bottom-2 left-0 right-0 flex justify-center">{leaderBoard}</div>
       }
     </div>
   )
