d7a7171583acb2527ebb0c918c44759f384cf839
renzivan <renzivanyapenguio@gmail.com>
2024-07-16 22:54:13 +0800

    fixed accordion width for large devices

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -12,7 +12,7 @@ const AccordionItem = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AccordionPrimitive.Item
     ref={ref}
-    className={cn("border-b w-fit", className)}
+    className={cn("border-b w-fit lg:w-full", className)}
     {...props}
   />
 ))
